b087f1682be1c8465036b5ad444bcbd1b377b6bc
Li Zhiqiang <kanysome@zju.edu.cn>
2012-11-17 20:08:25 +0800

    make DomainEquals.java support Map and List

--- src/main/java/zju/lzq/util/DomainEquals.java
+++ src/main/java/zju/lzq/util/DomainEquals.java
@@ -4,6 +4,10 @@ import java.beans.IntrospectionException;
 import java.beans.Introspector;
 import java.beans.PropertyDescriptor;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.apache.log4j.Logger;
 
@@ -22,13 +26,23 @@ public class DomainEquals {
 			return false;
 		}
 
-		Class<?> clazz = source.getClass();
-		PropertyDescriptor[] properties = getProperties(clazz);
-
-		if (!judgeWrap(source, target, clazz)) {
-			return false;
+		if (isWarp(source.getClass())) {
+			return judgeWrap(source, target);
 		}
 
+		if (source instanceof Map<?, ?>) {
+			return judgeMap(source, target);
+		}
+
+		if (source instanceof List<?>) {
+			return judgeList(source, target);
+		}
+
+		return judgeOther(source, target);
+	}
+
+	private static boolean judgeOther(Object source, Object target) {
+		PropertyDescriptor[] properties = getProperties(source.getClass());
 		for (PropertyDescriptor property : properties) {
 			Method method = property.getReadMethod();
 			try {
@@ -38,7 +52,7 @@ public class DomainEquals {
 					if (!method.invoke(source, new Object[] {}).equals(method.invoke(target, new Object[] {}))) {
 						return false;
 					}
-				} else if (property.getPropertyType().getDeclaredFields().length == 0) {
+				} else if (isWarp(property.getPropertyType())) {
 					if (!method.invoke(source, new Object[] {}).toString()
 							.equals(method.invoke(target, new Object[] {}).toString())) {
 						return false;
@@ -51,18 +65,11 @@ public class DomainEquals {
 				log.warn("fail to compare source and target", e);
 			}
 		}
-
 		return true;
 	}
 
-	private static boolean judgeWrap(Object source, Object target, Class<?> clazz) {
-		boolean result = true;
-		if (isWarp(clazz)) {
-			if (!source.toString().equals(target.toString())) {
-				result = false;
-			}
-		}
-		return result;
+	private static boolean judgeWrap(Object source, Object target) {
+		return source.toString().equals(target.toString());
 	}
 
 	private static boolean isWarp(Class<?> clazz) {
@@ -71,6 +78,44 @@ public class DomainEquals {
 		} catch (Exception e) {
 			return false;
 		}
+	}
+
+	@SuppressWarnings("unchecked")
+	private static boolean judgeMap(Object source, Object target) {
+		Map<Object, Object> sourceMap = (HashMap<Object, Object>) source;
+		Map<Object, Object> targetMap = (HashMap<Object, Object>) target;
+		if (sourceMap.size() != targetMap.size()) {
+			return false;
+		}
+
+		for (Object key : sourceMap.keySet()) {
+			Object sourceValue = sourceMap.get(key);
+			Object targetValue = targetMap.get(key);
+			boolean flag = assertEquals(sourceValue, targetValue);
+			if (!flag) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	@SuppressWarnings("unchecked")
+	private static boolean judgeList(Object source, Object target) {
+		List<Object> sourceList = (ArrayList<Object>) source;
+		List<Object> targetList = (ArrayList<Object>) target;
+		if (sourceList.size() != targetList.size()) {
+			return false;
+		}
+
+		for (int i = 0; i < sourceList.size(); ++i) {
+			Object sourceValue = sourceList.get(i);
+			Object targetValue = targetList.get(i);
+			boolean flag = assertEquals(sourceValue, targetValue);
+			if (!flag) {
+				return false;
+			}
+		}
+		return true;
 	}
 
 	private static PropertyDescriptor[] getProperties(Class<?> clazz) {
