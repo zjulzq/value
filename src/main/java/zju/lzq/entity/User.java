1acc8f2f9716d838710224f66531dcdd955318c2
Li Zhiqiang <kanysome@zju.edu.cn>
2012-11-21 00:06:55 +0800

    add a field to User.java

--- src/main/java/zju/lzq/entity/User.java
+++ src/main/java/zju/lzq/entity/User.java
@@ -3,10 +3,16 @@ package zju.lzq.entity;
 import java.io.Serializable;
 import java.util.Date;
 
-public class User implements Serializable{
-	
+public class User implements Serializable {
+
+	public static final int AUTH_ORIDINARY = 0;
+	public static final int AUTH_MANAGER = 1;
+	public static final int AUTH_GENERALMANAGER = 2;
+	public static final int AUTH_ADMIN = 3;
+
 	private static final long serialVersionUID = -5586714168499316384L;
 	private long id;
+	private String userName;
 	private Name name;
 	private String password;
 	private int auth;
@@ -18,6 +24,14 @@ public class User implements Serializable{
 
 	public void setId(long id) {
 		this.id = id;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
 	}
 
 	public Name getName() {
